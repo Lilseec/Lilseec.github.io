bf5415d23be46f089857ba714b20ac67356ef7c8
Yurka2048 <44337171+Yurka2048@users.noreply.github.com>
2021-05-05 23:20:46 +0300

    Fixed bug with NaN

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let input = document.getElementById('input');
 let button = document.getElementById('button');
-let i = 1;
+let i;
 
 function increaser() {
     let money = document.getElementById('amount').textContent;
     let amount = parseInt(money, 10);
     i = parseInt(input.value, 10);
+    if (isNaN(i)) {
+        i = 0;
+    }
     document.getElementById(`amount`).innerHTML = amount + i;
     amount = amount + i;
 }
